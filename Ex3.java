4306f58bc481e5744e0436465d6a3a5166a22e7f
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-06-29 11:41:08 +0700

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class Ex3 {
+import java.lang.Math;
+public class Ex23 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("a: ");
-        int a = scanner.nextInt();
-        System.out.print("b: ");
-        int b = scanner.nextInt();
-        System.out.print("c: ");
-        int c = scanner.nextInt();
-        System.out.print("d: ");
-        int d = scanner.nextInt();
-        System.out.printf("Result: %d/%d ",a*d+b*c,b*d);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        while(n!=0){
+            sum+=Math.abs(n%10);
+            n=n/10;
+        }
+        System.out.printf("Tổng các chữ số là: %d",sum);
     }
 }
