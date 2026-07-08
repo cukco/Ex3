51158c0170513e140cbbacb31cc13a89f180cf42
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-08 17:12:05 +0700

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
-public class Ex53 {
+public class Ex73 {
+    private static double rate;
+    static void setRate(double r){
+        rate=r;
+    }
+    static double getRate(){
+        return rate;
+    }
+    static double toUSD(int vnd){
+        return vnd*rate;
+    }
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Nhập mật khẩu: ");
-        String pass=sc.nextLine().trim();
-        String passwordRegex="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
-        if(pass.matches(passwordRegex)) System.out.print("Mật khẩu hợp lệ");
-        else System.out.print("Mật khẩu không hợp lệ");
+        setRate((double)1/27000);
+        System.out.printf("Tỷ giá hiện tại: 1 VND/ %f USD\n",rate);
+        System.out.printf("Đổi tiền: %d VND -> %.3f USD",5000000,toUSD(5000000));
     }
 }
