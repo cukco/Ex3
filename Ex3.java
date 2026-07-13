2c03139aa2386573cf0b71b25e78558fda95d001
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-13 13:39:52 +0700

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,22 +1,17 @@
-class Student{
-    String StudentId;
-    String StudentName;
-    int age;
-    public void displayData(){
-        System.out.print("ID: "+this.StudentId+",Name: "+this.StudentName+",Age: "+this.age);
+public class Ex73 {
+    private static double rate;
+    static void setRate(double r){
+        rate=r;
     }
-}
-public class Ex771 {
+    static double getRate(){
+        return rate;
+    }
+    static double toUSD(int vnd){
+        return vnd*rate;
+    }
     public static void main(String[] args){
-        Student A=new Student();
-        A.StudentId="2022";
-        A.StudentName="Yolo";
-        A.age=20;
-        Student B=new Student();
-        B.StudentId="20223";
-        B.StudentName="Oke";
-        B.age=21;
-        A.displayData();
-        B.displayData();
+        setRate((double)1/27000);
+        System.out.printf("Tỷ giá hiện tại: 1 VND/ %f USD\n",rate);
+        System.out.printf("Đổi tiền: %d VND -> %.3f USD",5000000,toUSD(5000000));
     }
 }
