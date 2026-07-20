8e8be87fef679b44d86b5773c89b84ce61c2228e
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-07-20 12:30:27 +0700

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,18 +1,85 @@
-    class Computer{
-        double calculatePrice(double basePrice){
-            return basePrice;
-        }
-        double calculatePrice(double basePrice, double tax){
-            return basePrice+tax;
-        }
-        double calculatePrice(double basePrice, double tax, double discount){
-            return basePrice+tax-discount;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Employee{
+    private String name;
+    private int id;
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    abstract double calculateSalary();
+    void showinfo(){
+        System.out.print("Name: "+name+", Id: "+id);
+    }
+}
+interface BonusEligible{
+    double calculateBonus();
+}
+class FulltimeEmployee extends Employee implements BonusEligible{
+    double baseSalary;
+    public FulltimeEmployee(String name, int id, double baseSalary){
+        super(name, id);
+        this.baseSalary = baseSalary;
+    }
+    @Override
+    double calculateSalary() {
+        return baseSalary;
+    }
+    @Override
+    public double calculateBonus(){
+        return baseSalary*0.1;
+    }
+    @Override
+    public void showinfo(){
+        super.showinfo();
+        System.out.print(", Base Salary: "+baseSalary);
+    }
+}
+class ParttimeEmployee extends Employee{
+    int workHours;
+    double hourlyRate;
+    public ParttimeEmployee(String name, int id, int workHours, double hourlyRate) {
+        super(name, id);
+        this.workHours = workHours;
+        this.hourlyRate = hourlyRate;
+    }
+    @Override
+    double calculateSalary() {
+        return workHours*hourlyRate;
+    }
+    @Override
+    public void showinfo(){
+        super.showinfo();
+        System.out.print(", Work Hours: "+workHours+", Hourly Rate: "+hourlyRate);
+    }
+}
+public class Ex3 {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(new FulltimeEmployee("John Doe", 23, 45.6));
+        employees.add(new ParttimeEmployee("Captain",29,39,3));
+        employees.add(new FulltimeEmployee("Shiet Dau", 21, 100));
+        employees.add(new ParttimeEmployee("Adudu",34,29,3));
+        for(int i=0;i<employees.size();i++){
+            employees.get(i).showinfo();
+            if(employees.get(i) instanceof BonusEligible){
+                System.out.println(", Bonus: "+((BonusEligible) employees.get(i)).calculateBonus());
+            }else if(employees.get(i) instanceof ParttimeEmployee){
+                System.out.println(", Salary: "+ employees.get(i).calculateSalary());
+            }
         }
     }
-    public class Ex103 {
-        public static void main(String[] args){
-            Computer computer=new Computer();
-            System.out.printf("Method 1: %.2f\nMethod 2: %.2f\nMethod 3: %.2f",computer.calculatePrice(100)
-            ,computer.calculatePrice(100,10),computer.calculatePrice(100,10,20));
-        }
-    }
+}
